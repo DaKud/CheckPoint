6882b2ef2c36e64a2016a8bbf9072a3e54fde2b7
DaKu <do.holyada@gmail.com>
2022-07-22 23:20:26 +0300

    Add TASK

---
+++ TASK/Program.cs
@@ -0,0 +1,13 @@
+﻿string[]String={"Hello", "2", "world", ";)"};
+for (int i = 0; i < String.Length; i++)
+{
+    int n=0;
+    if (String[i].Length <= 3)
+    {
+       String[n]=String[i];
+       Console.Write("'");
+       Console.WriteLine(String[n]+ "'");
+       Console.Write(" ");
+       n++;
+    }
+}
